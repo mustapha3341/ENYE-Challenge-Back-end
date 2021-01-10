79cc1b13dd4322eb0c3ead96b6a77ba6e7b6e56f
Sani Mustapha <mustapha3341@gmail.com>
2021-01-10 03:21:39 +0000

    added 404 route

--- server.js
+++ server.js
@@ -31,6 +31,10 @@ app.get("/api/rates/:baseCurrency/:currency", async (req, res, next) => {
     }
 });
 
+app.get("*", function (req, res) {
+    res.status(404).send("404 | Not Found!");
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}....`);
 });
