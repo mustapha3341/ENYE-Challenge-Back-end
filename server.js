ad90dbcde95db4f12cae974b0c3546c14f3c748c
Sani Mustapha <mustapha3341@gmail.com>
2021-01-10 03:12:08 +0000

    completed task

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const PORT = process.env.PORT || 4005;
+
+app.use(express.json());
+
+app.get("/api/rates/:baseCurrency/:currency", async (req, res, next) => {
+    try {
+        const { baseCurrency, currency } = req.params;
+
+        const response = await axios.get(
+            `https://api.exchangeratesapi.io/latest?base=${baseCurrency}&symbols=${currency}`
+        );
+
+        const { rates, date, base } = response.data;
+
+        res.json({
+            results: {
+                base,
+                date,
+                rates,
+            },
+        });
+    } catch (error) {
+        return res.status(400).json({
+            status: false,
+            message: error.message,
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}....`);
+});
